d59e54e329494bcdd545e870ca1b5c5f5478c23f
JFSR00 <juanfsr00@gmail.com>
2020-10-08 17:43:57 +0200

    Implementación de funciones

--- n_puzzle/main.c
+++ n_puzzle/main.c
@@ -5,4 +5,16 @@
  *      Author: Juan Francisco
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "puzle.h"
+
+int main(){
+	tEstado puzle*=crearEstado(puzle_inicial);
+
+	dispEstado(puzle);
+
+	return 0;
+}
