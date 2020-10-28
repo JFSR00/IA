61689e55f12c1d1fb0db7d1d084aeda445ff6437
'JFSR00' <'juanfsr00@gmail.com'>
2020-10-28 13:29:02 +0100

    Creación del Puzle de 3 piezas

---
+++ puzle3piezas/listaia.h
@@ -0,0 +1,42 @@
+/*******************************************/
+/*             LISTAIA.H                   */
+/*                                         */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef PUZLE_H_
+#include "puzle.h"
+#endif
+#ifndef BUSQUEDA_H_
+#include "busqueda.h"
+#endif
+
+#define VACIA NULL
+
+typedef struct ListaNodos {
+    struct ListaNodos *next;
+    int nodo[];
+    
+}ListaNodos;
+
+typedef struct ListaNodos *LISTA;
+
+tEstado* buscarElto(LISTA *c, tEstado *n);
+
+int esVacia( LISTA c);
+void InsertarPrimero(LISTA *c, void *n, int size);
+//void PopL(LISTA *c, void *n, int size);
+
+void ExtraerPrimero(LISTA c, void *n, int size);
+void EliminarPrimero(LISTA *c);
+
+void InsertarUltimo(LISTA *c, void *n, int size);
+LISTA Concatenar(LISTA l1,LISTA l2);
+
+
+
+
---
+++ puzle3piezas/main.c
@@ -0,0 +1,101 @@
+/*
+ * main.c
+ *
+ *  Created on: 7 oct. 2020
+ *      Author: Juan Francisco
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "puzle.h"
+#include "listaia.h"
+#include "busqueda.h"
+
+void printFila(tEstado* s);
+void printCol(tEstado* s);
+
+//void busqueda(tEstado* s);
+
+int main(){
+    //solucionFin(busquedaAnch());
+    //solucionFin(busquedaProf());
+    //solucionFin(busquedaAnchEstRep());
+    //solucionFin(busquedaProfEstRep());
+    solucionFin(busquedaAnchLimite(3));
+    solucionFin(busquedaProfLimite(4));
+    return 0;
+}
+/*int main(){
+	//pruebas del c�digo generado
+	int op;
+	tEstado *n,*a;
+	a=estadoInicial();
+
+	for(op=1; op<=NUM_OPERADORES; op++){
+		dispOperador(op);
+		if(esValido(op, a)){
+			n=aplicaOperador(op,a);
+			dispEstado(n);
+		}else{
+			printf("\n Operador no v�lido\n");
+		}
+	}
+}*/
+
+/*
+// Main de pruebas
+int main(){
+	tEstado *puzle=crearEstado(puzle_inicial), *puzle2=crearEstado(puzle_inicial), *final=crearEstado(puzle_final);
+	dispEstado(puzle);
+	printFila(puzle);
+	printCol(puzle);
+
+	printf("\nSon iguales los puzles? %i\n",iguales(puzle, puzle2));
+	printf("\nEs el objetivo? %i\n",testObjetivo(puzle));
+	printf("\nEs el objetivo? %i\n",testObjetivo(final));
+
+	printf("\n%i\n",esValido(ARRIBA,puzle));
+	puzle=aplicaOperador(ARRIBA,puzle);
+	dispEstado(puzle);
+	printFila(puzle);
+	printCol(puzle);
+
+	printf("\n%i\n",esValido(ARRIBA,puzle));
+	puzle=aplicaOperador(ARRIBA,puzle);
+	dispEstado(puzle);
+	printFila(puzle);
+	printCol(puzle);
+
+	printf("\n%i\n",esValido(ARRIBA,puzle));
+	if(esValido(ARRIBA,puzle)){
+		puzle=aplicaOperador(ARRIBA,puzle);
+		dispEstado(puzle);
+		printFila(puzle);
+		printCol(puzle);
+	}
+
+	printf("\nSon iguales los puzles? %i\n",iguales(puzle, puzle2));
+
+	return 0;
+}
+*/
+
+void printFila(tEstado* s){
+	printf("\nFilas: ");
+	for(int i=0; i<N*N; i++){
+		printf("%i ",s->fila[i]);
+	}
+}
+void printCol(tEstado* s){
+	printf("\nColumnas: ");
+	for(int i=0; i<N*N; i++){
+		printf("%i ",s->col[i]);
+	}
+}
+
+/*void busqueda(tEstado* s){
+
+
+}*/
